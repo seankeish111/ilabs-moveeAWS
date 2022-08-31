9ac473889090d02c8a37caf6f0e864e3fc2bc862
seankeish111 <shankesh.s@outlook.com>
2022-08-31 17:40:58 +1000

    Login UI implemented

--- lib/screens/authGate.dart
+++ lib/screens/authGate.dart
@@ -1,10 +1,10 @@
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
-import 'package:intelbis_removalist_platform/main.dart';
 import 'package:intelbis_removalist_platform/screens/enquiryForm.dart';
-
 import '../amplifyconfiguration.dart';
+import 'package:amplify_authenticator/amplify_authenticator.dart';
+
 
 
 
@@ -12,32 +12,37 @@ class authGate extends StatefulWidget {
   const authGate({Key? key}) : super(key: key);
 
   @override
-  State<authGate> createState() => _authGate();
+  State<authGate> createState() => _authGateState();
 }
 
-class _authGate extends State<authGate> {
+class _authGateState extends State<authGate> {
   @override
   void initState() {
     super.initState();
     _configureAmplify();
   }
 
-  Future<void> _configureAmplify() async {
+  void _configureAmplify() async {
     try {
       final auth = AmplifyAuthCognito();
-      await Amplify.addPlugin(auth);
-
-      // call Amplify.configure to use the initialized categories in your app
+      await Amplify.addPlugin(AmplifyAuthCognito());
       await Amplify.configure(amplifyconfig);
+      await Amplify.addPlugin(auth);
+      print('Successfully configured');
     } on Exception catch (e) {
-      print('An error occurred configuring Amplify: $e');
+      print('Error configuring Amplify:$e');
+
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: TodosPage(),
+    return  Authenticator(
+      child: MaterialApp(
+        builder: Authenticator.builder(),
+        home: const TodosPage(),
+      )
+
     );
   }
 }
